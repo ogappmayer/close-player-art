af0b130b328ea301464f82f3dd5c84ddf9de8c6a
Glen Simon <glen.a.simon@gmail.com>
2022-10-06 18:35:25 -0400

    Verify foundry v10 and format scripts

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,12 +1,12 @@
-import constants from '../Constants.js';
+import constants from "../Constants.js";
 
 export const registerSettings = () => {
-    game.settings.register(constants.modName, 'hotkey', {
-        name: `${constants.modName}.settings.hotkey.name`,
-        hint: `${constants.modName}.settings.hotkey.hint`,
-        scope: 'world',
-        config: true,
-        default: "`",
-        type: String,
-    });
+   game.settings.register(constants.modName, "hotkey", {
+      name: `${constants.modName}.settings.hotkey.name`,
+      hint: `${constants.modName}.settings.hotkey.hint`,
+      scope: "world",
+      config: true,
+      default: "`",
+      type: String,
+   });
 };
